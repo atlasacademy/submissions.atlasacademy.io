372bd359e42fd91a491c94dcc1da2ecf5992bbde
sadisticsolution <sadisticsolutione@gmail.com>
2019-06-06 10:24:23 -0700

    Reset upload status on export submission command

--- app/Console/Commands/ExportSubmissionCommand.php
+++ app/Console/Commands/ExportSubmissionCommand.php
@@ -2,6 +2,7 @@
 
 use App\Jobs\ExportSubmissionJob;
 use Illuminate\Contracts\Bus\Dispatcher;
+use Submission\SubmissionRepository;
 
 class ExportSubmissionCommand extends Command
 {
@@ -14,17 +15,31 @@ class ExportSubmissionCommand extends Command
      * @var Dispatcher
      */
     private $dispatcher;
+    /**
+     * @var SubmissionRepository
+     */
+    private $submissionRepository;
 
-    public function __construct(Dispatcher $dispatcher)
+    public function __construct(Dispatcher $dispatcher,
+                                SubmissionRepository $submissionRepository)
     {
         parent::__construct();
 
         $this->dispatcher = $dispatcher;
+        $this->submissionRepository = $submissionRepository;
     }
 
     public function handle()
     {
         $receipt = $this->ask('Input submission receipt');
+
+        $submission = $this->submissionRepository->getSubmission($receipt);
+        if (!$submission) {
+            $this->output->text("Submission not found.");
+            return;
+        }
+
+        $this->submissionRepository->setUploaded($receipt, false);
 
         $this->output->text("Dispatching export job ...");
         $this->dispatcher->dispatch(new ExportSubmissionJob($receipt));
