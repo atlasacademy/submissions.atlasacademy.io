5c13918c0d90b4bdf36afeb7a3b4c5b083ae2d6c
sadisticsolution <sadisticsolutione@gmail.com>
2020-09-14 02:43:29 -0700

    oops

--- app/Http/Controllers/SubmitRunController.php
+++ app/Http/Controllers/SubmitRunController.php
@@ -198,7 +198,7 @@ class SubmitRunController extends Controller
 
             // Only queue job if submitter provided their name. Otherwise, review submission first
             // if ($submitter) {
-            //     $this->dispatcher->dispatch(new ExportSubmissionJob($receipt));
+            $this->dispatcher->dispatch(new ExportSubmissionJob($receipt));
             // }
         }
 
